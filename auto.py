7d9ba6f4714c08f9071522e8cc68dbee41cafaf5
Xiaowei Chu <caaron@vmware.com>
2021-02-23 13:44:11 -0800

    improve the performance

--- auto.py
+++ auto.py
@@ -15,62 +15,66 @@ for line in lines:
     parts = line.split(',')
     pg_locks.add(parts[3][1:-1])
 
-pg_locks = list(pg_locks)
+overall_total_acquires_finisher = 0
+overall_total_acquires_osd = 0
+overall_total_contended_finisher = 0
+overall_total_contended_osd = 0
+overall_total_wait_time_finsiher = 0
+overall_total_wait_time_osd = 0
 
-total_acquires = {}
-total_contended = {}
-for pg_lock in pg_locks:
-    os.system('grep lock_acquire ' + source_file + '|grep ' + pg_lock +' >' + pg_lock)
-    f = open(pg_lock)
-    lines = f.readlines()
-    total_acquires[pg_lock] = len(lines)
-    f.close()
-    os.system('grep lock_contended ' + source_file + '|grep ' + pg_lock +' >' + pg_lock)
-    f = open(pg_lock)
-    lines = f.readlines()
-    total_contended[pg_lock] = len(lines)
-    f.close()
-    os.system('sudo rm ' + pg_lock)
+f = open(source_file)
+lines = f.readlines()
+for line in lines:
+    parts = line.split(',')
+    if len(parts) != 4:
+        continue
+    parts = parts[3]
+    if parts[1:-1] in pg_locks and 'lock_acquire' in line and 'finisher' in line:
+        overall_total_acquires_finisher += 1
+        continue
+    if parts[1:-1] in pg_locks and 'lock_acquire' in line and 'tp_osd_tp' in line:
+        overall_total_acquires_osd += 1
+        continue
+    if parts[1:-1] in pg_locks and 'lock_contended' in line and 'finisher' in line:
+        overall_total_contended_finisher += 1
+        continue
+    if parts[1:-1] in pg_locks and 'lock_contended' in line and 'tp_osd_tp' in line:
+        overall_total_contended_osd += 1
+        continue
 
 os.system('sudo rm lock_addresses')
 
 os.system("grep 'Mutex Address' " + source_file + " -A 5000 >" + output_file)
 
-avg_wait_time = {}
-
+pg_locks = list(pg_locks)
+total_wait_time_finisher = {}
+total_wait_time_osd = {}
 for pg_lock in pg_locks:
     f = open(output_file)
     lines = f.readlines()
-    total_time = 0
+    total_time_finisher = 0
+    total_time_osd = 0
     for line in lines:
-        if pg_lock in line:
+        if pg_lock in line and 'finisher' in line:
             parts = line.split(',')
-            total_time = total_time +  int(parts[3])
+            total_time_finisher = total_time_finisher +  int(parts[3])
+            continue
+        if pg_lock in line and 'tp_osd_tp' in line:
+            parts = line.split(',')
+            total_time_osd = total_time_osd +  int(parts[3])
     f.close()
-    avg_time = total_time / total_contended[pg_lock]
-    avg_wait_time[pg_lock] = avg_time
+    total_wait_time_finisher[pg_lock] = total_time_finisher
+    total_wait_time_osd[pg_lock] = total_time_osd
 
 
-
-print(total_acquires)
-print(total_contended)
-print(avg_wait_time)
-
-overall_total_acquires = 0
-overall_total_contended = 0
-overall_avg_wait_time = 0
-
-total_wait_time = 0
 for pg_lock in pg_locks:
-    overall_total_acquires += total_acquires[pg_lock]
-    overall_total_contended += total_contended[pg_lock]
-    total_wait_time += avg_wait_time[pg_lock] * total_contended[pg_lock]
+    overall_total_wait_time_finsiher += total_wait_time_finisher[pg_lock]
+    overall_total_wait_time_osd += total_wait_time_osd[pg_lock]
 
-overall_avg_wait_time = total_wait_time / overall_total_contended
-
-print('overall_total_acquires:' + str(overall_total_acquires))
-print('overall_total_contended:' + str(overall_total_contended))
-print('overall_contention_ratio:' + str(overall_total_contended/overall_total_acquires))
-print('overall_avg_wait_time:' + str(overall_avg_wait_time))
+print('Thread_Name, PG_lock_acquires, PG_lock_contentions, Total_wait_time, Average_wait_time')
+print('finisher,' + str(overall_total_acquires_finisher) + ',' + str(overall_total_contended_finisher)+ ',' +  str(overall_total_wait_time_finsiher)+ ',' +  str(overall_total_wait_time_finsiher/overall_total_contended_finisher))
+print('tp_osd_tp,' + str(overall_total_acquires_osd)+ ',' +  str(overall_total_contended_osd)+ ',' +  str(overall_total_wait_time_osd)+ ',' +  str(overall_total_wait_time_osd/overall_total_contended_osd))
+print('')
+print('Number of PG locks involved:' + str(len(pg_locks)))
 
 
