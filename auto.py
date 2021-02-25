abe8ff8c028e2066358efeed63d7dc26763741cb
Xiaowei Chu <caaron@vmware.com>
2021-02-24 22:13:24 -0800

    fixed some bugs

--- auto.py
+++ auto.py
@@ -5,7 +5,7 @@ source_file = sys.argv[1]
 output_file = sys.argv[2]
 
 os.system('grep PG4lock '+ source_file  +' -B 6 -C 6 > lock_addresses')
-os.system('grep lock_contended lock_addresses > lock_addresses.1')
+os.system('grep Callstack lock_addresses > lock_addresses.1')
 os.system('mv lock_addresses.1 lock_addresses')
 
 f = open('lock_addresses')
@@ -13,7 +13,10 @@ lines = f.readlines()
 pg_locks = set()
 for line in lines:
     parts = line.split(',')
-    pg_locks.add(parts[3][1:-1])
+    part = parts[3]
+    parts = part.split(' ')
+    part = parts[2]
+    pg_locks.add(part)
 
 overall_total_acquires_finisher = 0
 overall_total_acquires_osd = 0
@@ -72,9 +75,17 @@ for pg_lock in pg_locks:
     overall_total_wait_time_osd += total_wait_time_osd[pg_lock]
 
 print('Thread_Name, PG_lock_acquires, PG_lock_contentions, Total_wait_time, Average_wait_time')
-print('finisher,' + str(overall_total_acquires_finisher) + ',' + str(overall_total_contended_finisher)+ ',' +  str(overall_total_wait_time_finsiher)+ ',' +  str(overall_total_wait_time_finsiher/overall_total_contended_finisher))
-print('tp_osd_tp,' + str(overall_total_acquires_osd)+ ',' +  str(overall_total_contended_osd)+ ',' +  str(overall_total_wait_time_osd)+ ',' +  str(overall_total_wait_time_osd/overall_total_contended_osd))
+if overall_total_wait_time_finsiher !=0:
+    print('finisher,' + str(overall_total_acquires_finisher) + ',' + str(overall_total_contended_finisher)+ ',' +  str(overall_total_wait_time_finsiher)+ ',' +  str(overall_total_wait_time_finsiher/overall_total_contended_finisher))
+else:
+    print('finisher,' + str(overall_total_acquires_finisher) + ',' + str(overall_total_contended_finisher)+ ',' +  str(overall_total_wait_time_finsiher)+ ',' +  str(0))
+
+if overall_total_wait_time_osd !=0:
+    print('tp_osd_tp,' + str(overall_total_acquires_osd)+ ',' +  str(overall_total_contended_osd)+ ',' +  str(overall_total_wait_time_osd)+ ',' +  str(overall_total_wait_time_osd/overall_total_contended_osd))
+else:
+    print('tp_osd_tp,' + str(overall_total_acquires_osd)+ ',' +  str(overall_total_contended_osd)+ ',' +  str(overall_total_wait_time_osd)+ ',' +  str(0))
+
 print('')
-print('Number of PG locks involved:' + str(len(pg_locks)))
+print('Number of PG locks used:' + str(len(pg_locks)))
 
 
